096cf11ca089516a4c4e51313cedc7d9f7818b82
Tim Smith <tsmith84@gmail.com>
2015-09-09 10:37:03 -0700

    Add long_description and supermarket metadata

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,9 @@
 name              'suicide_control'
 maintainer        'Chef Software, Inc.'
-maintainer_email  'sysadmin@getchef.com'
+maintainer_email  'sysadmin@chef.io'
 license           'Apache 2.0'
+long_description   IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '2.0.0'
+
+source_url 'https://github.com/chef-cookbooks/suicide_control' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/suicide_control/issues' if respond_to?(:issues_url)
