a174f5dad511c86dc3f1fbf5993aa5c80fed0d90
Tim Smith <tsmith84@gmail.com>
2016-04-28 17:02:19 -0700

    Testing updates

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name              'suicide_control'
 maintainer        'Chef Software, Inc.'
-maintainer_email  'sysadmin@chef.io'
+maintainer_email  'cookbooks@chef.io'
 license           'Apache 2.0'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.0.0'
