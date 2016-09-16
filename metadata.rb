bc94895a708484a0c96a1a1773646e5d3392b0eb
Tim Smith <tsmith@chef.io>
2016-09-16 11:12:14 -0700

    Require Chef 12.1+
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -5,5 +5,6 @@ license           'Apache 2.0'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.0.0'
 
-source_url 'https://github.com/chef-cookbooks/suicide_control' if respond_to?(:source_url)
-issues_url 'https://github.com/chef-cookbooks/suicide_control/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/chef-cookbooks/suicide_control'
+issues_url 'https://github.com/chef-cookbooks/suicide_control/issues'
+chef_version '>= 12.1'
